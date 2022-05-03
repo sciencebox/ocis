5ff502c73293f396c9453d19bdb8d9792566db07
Enrico Bocchi <enrico.bocchi@cern.ch>
2022-05-03 13:25:14 +0200

    ocis: Bump OCIS_IMAGE_VERSION to 1.20.0

--- build.sh
+++ build.sh
@@ -4,7 +4,8 @@
 BASE_TAG='gitlab-registry.cern.ch/sciencebox/docker-images/ocis'
 
 # Specify cvmfs version to be installed (or comment out to use the latest)
-OCIS_IMAGE_VERSION='1.11.0'
+OCIS_IMAGE_VERSION='1.11.0'	#digest: sha256:a34eafe3390920ed36a91d080b35e41d389c8fab71b4f2676fee07a845a5fda8
+OCIS_IMAGE_VERSION='1.20.0'	#digest: sha256:b75288e78ee3192e97261b6de85a11a67dbbca97fe46624aaaa74a62e77770bb
 
 # Build the Docker image
 if [ -z $OCIS_IMAGE_VERSION ]; then
